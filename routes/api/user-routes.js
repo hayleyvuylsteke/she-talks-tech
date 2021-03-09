f460ca6b82d9b994262342329ca771b7beff006c
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-08 19:22:27 -0500

    user routes added

---
+++ routes/api/user-routes.js
@@ -0,0 +1,88 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Gets all users from the user table in the database and sends back in JSON
+router.get('/', (req, res) => {
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+// Gets a specific user from the user table in the database and sends back in JSON
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'Sorry! There was no user found.'});
+            return
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// Creates a new user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// Updates a user
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'Sorry! There was no user found.'})
+            return
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+});
+
+// deletes a user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'Sorry! There was no user found.'})
+            return
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+});
+
+module.exports = router;
