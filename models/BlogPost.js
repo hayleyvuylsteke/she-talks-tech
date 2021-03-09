e864a004633e05e36ef0135e6e38bac81cb13107
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-09 07:23:04 -0500

    blog post model added

---
+++ models/BlogPost.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class BlogPost extends Model {}
+
+BlogPost.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      post_title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+      },
+      post_content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [60]
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+
+  module.exports = BlogPost;
