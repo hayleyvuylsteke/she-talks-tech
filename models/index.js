3a061ad04658896689840fce3330420c358b2901
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-09 07:27:18 -0500

    model associations defined

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
-const User = require('./User');
+const User = require('./User')
+const BlogPost = require('./BlogPost')
 
-module.exports = { User };
+//model associations
+
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id'
+})
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+module.exports = { User, BlogPost }
+
