2519adb617f64494a737ba90705e76135ff8edb1
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-13 11:14:50 -0500

    updates to timeout

--- public/javascript/auto-logout.js
+++ public/javascript/auto-logout.js
@@ -1,26 +1,58 @@
-function autoLogout() {
-    this.timeout = setTimeout(() => {
-            const response = await fetch('/api/users/logout', {
-                method: 'post',
-                headers: { 'Content-Type': 'application/json' }
-          });
-          
-          if (response.ok) {
-            document.location.replace('/');
-          } else {
-            alert(response.statusText);
-          }
-    }, 60000)
+var autoLogout = (async function() {
+  function AutoLogout() {
+    this.events = ['load', 'mousemove', 'mousedown' ,'click', 'scroll', 'keypress']
 
-//list events
- const events = ['load', 'mousemove', 'mousedown','click', 'scroll', 'keypress'];
+    this.logout = this.logout.bind(this)
+    this.resetTimeout = this.resetTimeout.bind(this)
 
- //create a timeout function
- const resetTimeout = () => {
-     clearTimeout(timeout);
-     setTimeout();
- }
+    var self = this
+    this.events.forEach(function(event) {
+      window.addEventListener(event, self.resetTimeout)
+    })
+    this.setTimeout
+  }
 
+  var _t = AutoLogout.prototype
 
- events.forEach(function(event) { window.addEventListener(event, resetTimeout) })
-}
+  _t.clearTimeout = function() {
+    if(this.logoutTimeout)
+    clearTimeout(this.logoutTimeout)
+  }
+
+  _t.setTimeout = function() {
+    this.logoutTimeout = setTimeout(this.logout, 30 *60 *1000)
+  }
+
+  _t.resetTimeout = function() {
+    this.clearTimeout()
+    this.setTimeout()
+  }
+
+  _t.logout = function() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+    alert(response.statusText);
+    }
+
+    this.destroy()
+  }
+
+  _t.destroy = function () {
+    this.clearTimeout();
+
+    var self = this
+    this.forEach(function(event){
+      window.removeEventListener(event, self.resetTimeout)
+    })
+  }
+
+  return AutoLogout
+})
+()
+
